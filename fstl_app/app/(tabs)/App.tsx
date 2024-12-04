7e21c96e54d9e03ed97120074c71b4f192539b9b
jeiooo <angzcolao@gmail.com>
2024-12-05 01:35:28 +0800

    commit 3

--- fstl_app/app/(tabs)/App.tsx
+++ fstl_app/app/(tabs)/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './index';
+import LoginScreen from './LoginScreen';
 import DashboardScreen from './DashboardScreen';
+import TestScreen from './testScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="DashboardScreen">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="DashboardScreen" component={DashboardScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Test" component={TestScreen} options={{ headerShown: false }} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
--- fstl_app/app/(tabs)/DashboardScreen.tsx
+++ fstl_app/app/(tabs)/DashboardScreen.tsx
@@ -1,11 +1,19 @@
-
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const DashboardScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the Dashboard</Text>
+      <Text style={styles.title}>Dashboard</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Test')}  // Navigate to ProfileScreen
+      >
+        <Text style={styles.buttonText}>Go to Profile</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -19,6 +27,18 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    padding: 15,
+    backgroundColor: '#007bff',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
     fontWeight: 'bold',
   },
 });
